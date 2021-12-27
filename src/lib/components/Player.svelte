7bc7d60b336d779838df77bf3aef6f0cc9ffc66a
jvanheeswijck <67222238+jvanheeswijck@users.noreply.github.com>
2021-12-27 12:47:45 +1030

    added scoring and player components

---
+++ src/lib/components/Player.svelte
@@ -0,0 +1,58 @@
+<script> 
+    export let data;
+        
+    $: score = data.fame + data.spells * 2 + data.actions + data.artifacts * 2 + Math.floor(data.crystals / 2) + data.healthy + Math.floor(data.wounded / 2) + data.fortified * 2 + data.adventure * 2 - data.wounds * 2 + data.cityleader * 7 + data.cityfollower * 4;
+    </script>
+    <div>
+        <h2>
+            Player {data.n}
+        </h2>
+        
+        <label>Fame:
+            <input type='number' min=0 bind:value={data.fame}>
+        </label>
+        <!-- Spells/Actions --> 
+        <label>Number of Spells:
+            <input type='number' min=0 bind:value={data.spells}>
+        </label>
+        <label>Number of Advanced Actions:
+            <input type='number' min=0 bind:value={data.actions}>
+        </label>
+    <!-- Artifacts/Crytals -->
+        <label>Number of Artifacts:
+            <input type='number' min=0 bind:value={data.artifacts}>
+        </label>
+        <label>Number of Crystals:
+            <input type='number' min=0 bind:value={data.crystals}>
+        </label>
+    <!-- Units/Wounded Units -->
+        <label>Total levels of Healthy Units:
+            <input type='number' min=0 bind:value={data.healthy}>
+        </label>
+        <label>Total levels of Wounded Units:
+            <input type='number' min=0 bind:value={data.wounded}>
+        </label>
+    <!-- Monasteries/Keeps/Mage Towers -->
+        <label>Number of Conquered Fortified Sites:
+            <input type='number' min=0 bind:value={data.fortified}>
+        </label>
+    <!-- Adventure sites -->
+        <label>Number of Conquered Adventure Sites:
+            <input type='number' min=0 bind:value={data.adventure}>
+        </label>
+    <!-- Wounds -->
+        <label>Number of Wounds:
+            <input type='number' min=0 bind:value={data.wounds}>
+        </label>
+    <!-- Cities Led/Followed -->
+        <label>Number of Cities Led :
+            <input type='number' min=0 bind:value={data.cityleader}>
+        </label>
+        <label>Number of Cities Followed :
+            <input type='number' min=0 bind:value={data.cityfollower}>
+        </label>
+        <p>
+            Score: {score}
+        </p>
+    </div>
+    
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,70 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Player from '$lib/components/Player.svelte'
+	
+	let name = 'whoever you are';
+	let numberOfPlayers = 0;
+	
+	$: players = Array(numberOfPlayers).fill({
+			fame: 0,
+			spells: 0,
+			actions: 0,
+			artifacts: 0,
+			crystals: 0,
+			healthy: 0,
+			wounded: 0,
+			fortified: 0,
+			adventure: 0,
+			wounds: 0,
+			cityleader: 0,
+			cityfollower: 0
+		})
+		.map((p, i) => ({
+			n: i + 1,
+			...p
+		}))
+	
+</script>
+<o>
+Hello {name}!
+</o>
+
+
+<p>
+ My name is Jesper and welcome to my Mage Knight scoring website! 
+	This is where you can do all the endgame scoring for competitive Mage Knight without having
+	to do all that annoying Maths.
+</p>
+
+
+
+<style>
+	p {
+		color: limegreen;
+		font-size: 1em;
+	}
+	
+	o {
+		color: turquoise;
+	}
+</style>
+
+<p>
+	Type your name below
+</p>
+
+<input type='text' bind:value={name}>
+
+
+<!-- Select number of players -->
+<label>Number of players:
+	<input type='number' min=0 max=4 bind:value={numberOfPlayers}>
+</label>
+
+<p> 
+	Click this button when you have finished scoring
+</p>
+	<button> Finished Scoring! </button>
+<!-- For each player, one number input for each category -->
+{#each players as player}
+	<Player bind:data={player}>  </Player>	
+{/each}
